5dce2f0417700375d0bbfbe7f6f6ef1496b0f5ed
evwilkin <evan.wilkinson@gmail.com>
2018-02-20 17:04:38 -0500

    Removed instructions comments from code

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -11,8 +11,9 @@ class SearchBooks extends Component {
   filterBooks = (query) => {
     if (query) {
       BooksAPI.search(query).then((results) => {
-        if (results) {
+        if (results && !results.error) {
           results.forEach((result) => {
+            // Check each book against existing shelves & assign shelf as needed
             for (var book in this.props.books) {
               if (result.id === this.props.books[book].id) {
                 result.shelf = this.props.books[book].shelf;
@@ -37,19 +38,10 @@ class SearchBooks extends Component {
           <div className='search-books-bar'>
             <Link to='/' className='close-search' />
             <div className='search-books-input-wrapper'>
-              {/*
-                NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                You can find these search terms here:
-                https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                you don't find a specific author or title. Every search is limited by search terms.
-              */}
-              <input
+              <input autoFocus
                 type='text'
                 placeholder='Search by title or author'
                 onChange={(e) => this.filterBooks(e.target.value)} />
-
             </div>
           </div>
           <div className='search-books-results'>
